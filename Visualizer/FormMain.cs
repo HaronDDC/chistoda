4c3e626a886b250921859522bf52c0b7fb04dc99
Дмитрий Кидяев <haronddc@gmail.com>
2019-07-27 23:33:33 +0300

    222

--- Visualizer/FormMain.cs
+++ Visualizer/FormMain.cs
@@ -90,16 +90,80 @@ namespace Visualizer
             var result = _result[selectedResult];
 
             chartMain.Series.Clear();
+
+            //var serAge = chartMain.Series.Add("Age");
+            //var serScore = chartMain.Series.Add("Score");
+
+            //serAge.Points.AddXY("Max", 1);
+            //serAge.Points.AddXY("Carl", 2);
+            //serAge.Points.AddXY("Fedor", 3);
+            //serAge.Points.AddXY("Sasha", 4);
+
+            //serScore.Points.AddXY("Max", 10);
+            //serScore.Points.AddXY("Carl", 20);
+            //serScore.Points.AddXY("Fedor", 30);
+            //serScore.Points.AddXY("Sasha", 40);
+            //return;
+
+            //List<string> listVeh = new List<string>() { "Age", "Score" };
+            //List<string> listLocation = new List<string>() { "Max", "Carl", "Fedor", "Sasha" };
+
+            //Dictionary<string, Dictionary<string, int>> aaa = new Dictionary<string, Dictionary<string, int>>();
+            //aaa["Max"] = new Dictionary<string, int>() { { "Age", 1 }, { "Score", 2 } };
+            //aaa["Carl"] = new Dictionary<string, int>() { { "Age", 3 }, { "Score", 4 } };
+            //aaa["Fedor"] = new Dictionary<string, int>() { { "Age", 5 }, { "Score", 6 } };
+            //aaa["Sasha"] = new Dictionary<string, int>() { { "Age", 7 }, { "Score", 8 } };
+
+            //foreach (var l in listLocation)
+            //{
+
+            //    foreach (var v in listVeh)
+            //    {
+            //        var ser = chartMain.Series.FindByName(v);
+            //        if (ser == null)
+            //        {
+            //            ser = chartMain.Series.Add(v);
+            //            ser.ChartType = SeriesChartType.Column;
+            //        }
+            //        ser.Points.AddXY(l, aaa[l][v]);
+            //    }
+            //}
+            //return;
+            Dictionary<string, int> pointIdx = new Dictionary<string, int>();
+            int locIdx = 0;
+            foreach (var loc in _task.Locations)
+            {
+                string locName = loc.LocationName;
+                pointIdx[locName] = locIdx;
+                foreach (var veh in _task.Vehicles)
+                {
+                    var ser = chartMain.Series.FindByName(veh.Name);
+                    if (ser == null)
+                    {
+                        ser = chartMain.Series.Add(veh.Name);
+                        ser.ChartType = SeriesChartType.Column;
+                    }
+                    ser.Points.AddXY(locName, 0);
+                }
+                locIdx++;
+            }
+
             foreach (var loc in result.LocResult)
             {
+                string locName = loc.Location.LocationName;
                 foreach (var vr in loc.VehicleResult)
                 {
                     var ser = chartMain.Series.FindByName(vr.Vehicle.Name);
                     if (ser == null)
+                    {
                         ser = chartMain.Series.Add(vr.Vehicle.Name);
-                    ser.Points.AddXY(loc.Location.LocationName, vr.Count);
+                        ser.ChartType = SeriesChartType.Column;
+                    }
+                    ser.Points[pointIdx[locName]].YValues = new double[] { vr.Count };
+                    //ser.Points.AddXY(locName, vr.Count);
                 }
             }
+
         }
     }
 }
